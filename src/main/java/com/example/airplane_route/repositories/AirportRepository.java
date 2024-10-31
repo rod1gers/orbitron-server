882acc375e2aabe322ea4af40646ba9bcf73fadb
rod1gers <rodgersochieng143@gmail.com>
2024-10-31 09:34:49 +0300

    prevent double entry of airport

--- src/main/java/com/example/airplane_route/repositories/AirportRepository.java
+++ src/main/java/com/example/airplane_route/repositories/AirportRepository.java
@@ -2,10 +2,10 @@ package com.example.airplane_route.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.airplane_route.models.Airport;
+import java.util.Optional;
 
 @Repository
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-    
+    Optional<Airport> findByName(String name);
 }
