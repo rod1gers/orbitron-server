5c67eaf8ca6751eb76b63b584c6065ade535ac9f
Rodgers Ochieng <rodgersochieng143@gmail.com>
2025-06-01 20:26:20 +0300

    Add websocket

--- src/main/java/com/orbitron/controllers/EngineParamController.java
+++ src/main/java/com/orbitron/controllers/EngineParamController.java
@@ -8,19 +8,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orbitron.entities.entities.Engine;
 import com.orbitron.repositories.EngineRepository;
+import com.orbitron.services.EngineSimulator;
 
 @RestController
 @RequestMapping("/api/v1/engine")
 public class EngineParamController {
-    @Autowired
-    private EngineRepository engineRepository;
+
+    private final EngineRepository engineRepository;
+    private final EngineSimulator engineSimulator;
+
+    public EngineParamController(EngineSimulator engineSimulator, EngineRepository engineRepository) {
+        this.engineSimulator = engineSimulator;
+        this.engineRepository = engineRepository;
+    }
+
     
     @GetMapping("/{id}")
     public ResponseEntity<Engine> getEngineParam(@PathVariable Long id) {
@@ -43,6 +51,13 @@ public class EngineParamController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body("Error saving engine");
         }
+    }
+
+    @PostMapping("/startEngine")
+    public ResponseEntity<String> startEngine(@RequestParam Long engineId) {
+        engineSimulator.startSpoolUp(engineId);
+
+        return ResponseEntity.ok("Spool-up started successfully");
     }
 
     // Add method to set parameters
