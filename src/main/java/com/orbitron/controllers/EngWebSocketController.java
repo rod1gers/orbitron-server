a5d67bf9091063eae67b08eb541cbaa297caa0a6
rod1gers <rodgersochieng143@gmail.com>
2025-09-16 06:53:58 +0300

    Add executor to handle the threads that will be listening for the data from Octave engine

--- src/main/java/com/orbitron/controllers/EngWebSocketController.java
+++ src/main/java/com/orbitron/controllers/EngWebSocketController.java
@@ -1,17 +1,21 @@
 package com.orbitron.controllers;
 
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import com.orbitron.objectModels.EngineStartRequest;
 import com.orbitron.services.EngineSimulator;
+import com.orbitron.services.OctaveTcpClient;
 
 @Controller
 public class EngWebSocketController {
     private final EngineSimulator engineSimulator;
+    private final OctaveTcpClient octaveTcpClient;
 
-    public EngWebSocketController(EngineSimulator engineSimulator) {
+    public EngWebSocketController(EngineSimulator engineSimulator, OctaveTcpClient octaveTcpClient) {
         this.engineSimulator = engineSimulator;
+        this.octaveTcpClient = octaveTcpClient;
     }
 
     @MessageMapping("/start-engine")
